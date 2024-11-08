e2f3db1e91f99e6b651f8adb4c9a02d5d3fad679
Ruben14f <mansilla8090@gmail.com>
2024-11-07 21:33:46 -0300

    ultimos ajustes faltan

--- js/funciones.js
+++ js/funciones.js
@@ -11,3 +11,47 @@ navLinks.forEach(link => {
         bootstrapCollapse.hide();
     });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const texts = ["¿Te intereso mi perfil?", "¡Te invito a contactarme!"];
+    let currentTextIndex = 0;
+    let currentCharIndex = 0;
+    const typewriterElement = document.getElementById("typewriter");
+
+    function typeText() {
+        if (currentCharIndex < texts[currentTextIndex].length) {
+            typewriterElement.textContent += texts[currentTextIndex].charAt(currentCharIndex);
+            currentCharIndex++;
+            setTimeout(typeText, 100); 
+        } else {
+            if (currentTextIndex === texts.length - 1) {
+                return; 
+            }
+            setTimeout(deleteText, 2000); 
+        }
+    }
+
+    function deleteText() {
+        if (currentCharIndex > 0) {
+            typewriterElement.textContent = typewriterElement.textContent.slice(0, -1);
+            currentCharIndex--;
+            setTimeout(deleteText, 50); 
+        } else {
+            currentTextIndex = (currentTextIndex + 1) % texts.length; 
+            setTimeout(typeText, 1000);
+        }
+    }
+
+   
+    typewriterElement.addEventListener("click", function() {
+        currentTextIndex = 0;
+        currentCharIndex = 0;
+        typewriterElement.textContent = "";
+        typeText(); 
+    });
+
+    typeText(); // Comienza la animación
+});
+
